3185d8aee060b365f6ea511423336ba7d3f5b6f4
Joel <joelkaplan1@gmail.com>
2019-05-02 12:14:22 -0400

    completed first two underbar

--- src/underbar.js
+++ src/underbar.js
@@ -6,7 +6,8 @@
   // Returns whatever value is passed as the argument. This function doesn't
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
-  _.identity = function(val) {
+  _.identity = function(val) { 
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : n > array.length ? array : array.slice(array.length-n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,7 +47,17 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
-  };
+    if (Array.isArray(collection)) {
+      for (var i=0; i<collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var i in collection) {
+        iterator(collection[i], i, collection);
+       }
+     }
+  }
+
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
